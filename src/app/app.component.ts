e49e55e004629947cadfd6834d180bd1b5228919
justin lowe <justin.lowe53@yahoo.com>
2019-10-08 11:16:48 -0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appName';
+  toggled: boolean = true;
+
+  handleClick() {
+    this.toggled = !this.toggled;
+  }
 }
